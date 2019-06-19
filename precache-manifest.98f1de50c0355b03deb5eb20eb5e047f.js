4745ca84d1d1fcdb732058eeb8c6248b877f0714
Léo Generet <lpgeneret@gmail.com>
2019-06-19 16:52:52 +0200

    Updates

---
+++ precache-manifest.98f1de50c0355b03deb5eb20eb5e047f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f17a3877167fd63b9f858489b5b45dc",
+    "url": "/MovieBuster/index.html"
+  },
+  {
+    "revision": "9ac328a8c15ceb0393f4",
+    "url": "/MovieBuster/static/css/2.7fad5d03.chunk.css"
+  },
+  {
+    "revision": "c620b2fd9b93aee77e43",
+    "url": "/MovieBuster/static/css/main.6a56adc5.chunk.css"
+  },
+  {
+    "revision": "9ac328a8c15ceb0393f4",
+    "url": "/MovieBuster/static/js/2.3b05097a.chunk.js"
+  },
+  {
+    "revision": "c620b2fd9b93aee77e43",
+    "url": "/MovieBuster/static/js/main.7c68691c.chunk.js"
+  },
+  {
+    "revision": "1033a073cae3e2f5baf1",
+    "url": "/MovieBuster/static/js/runtime~main.c30275fe.js"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/MovieBuster/static/media/ProductSansBold.dba0c688.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/MovieBuster/static/media/ProductSansRegular.eae9c18c.ttf"
+  }
+]);
